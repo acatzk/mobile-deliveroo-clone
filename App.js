ae18f23f7e81c70c16d3df4bfb4636c21cb78387
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-01-11 18:22:19 +0800

    Feature: Install & Setup Tailwindcss (#2)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
+import { Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex h-screen flex-1 items-center justify-center text-black">
+      <Text className="text-black">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+export default App
